99cbb55a80d602a801b270b57eb9bc140a8ea309
Gatlingover <Samsonf21@gmail.com>
2017-08-08 12:50:42 +0100

    C# for beginners, the calculator

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace calculator
 {
-    class Program
+    class MainClass
     {
         static void Main(string[] args)
         {
+            int num01;
+            //float decimalpoint = 102.3; 
+            double decimalpoint02 = 10.444; //float with more precision
+            bool  trueOrFalse = true;
+            string sentence = "Hello World";
+
+            int num02;
+
+            Console.Write("type a number to be multipled:");
+            num01 = Convert.ToInt32 (Console.ReadLine());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result is " + num01 * num02);
+            Console.ReadKey();
         }
     }
 }
